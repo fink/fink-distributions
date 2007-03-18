78fc3b738204faa0ee80675ea751b604ea05c526
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-03-18 14:06:00 +0000

    fix library-ordering issues

--- 10.4/unstable/find-bad-stuff.sh
+++ 10.4/unstable/find-bad-stuff.sh
@@ -12,7 +12,7 @@ BAD_STRING="$1"; shift
 SEARCH_PATH="$1"; shift
 [ -z "$SEARCH_PATH" ] && SEARCH_PATH="$FINKROOT"
 
-find "$SEARCH_PATH" -type f | grep -v -E '\.(sh|pl)$' | while read FILE; do
+find "$SEARCH_PATH" "$@" -type f | grep -v -E '\.(sh|pl)$' | while read FILE; do
 	if [ `file "$FILE" | grep -c Mach` -gt 0 ]; then
 		otool -L "$FILE" | while read LINE; do
 			if [ `echo "$LINE" | grep -c -E "$BAD_STRING"` -gt 0 ]; then
